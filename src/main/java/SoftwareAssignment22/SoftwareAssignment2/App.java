176a8e7a3862dec64bd0bc12e2f16a08ca9f6e5a
PatrykKozak <patrykkozak1998@gmail.com>
2019-11-19 12:49:47 +0000

    initial commit

---
+++ src/main/java/SoftwareAssignment22/SoftwareAssignment2/App.java
@@ -0,0 +1,13 @@
+package SoftwareAssignment22.SoftwareAssignment2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
