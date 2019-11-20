ad030dbb2c4ebd112ad853b9ffc516b2f18f98fa
PatrykKozak <patrykkozak1998@gmail.com>
2019-11-20 15:54:22 +0000

    adding to course programme class

--- src/main/java/SoftwareAssignment22/SoftwareAssignment2/CourseProgramme.java
+++ src/main/java/SoftwareAssignment22/SoftwareAssignment2/CourseProgramme.java
@@ -8,5 +8,35 @@ public class CourseProgramme {
 	private String listOfStundentsEnrolled[];
 	private DateTime startDate;
 	private DateTime closeDate;
+	public String getClassName() {
+		return className;
+	}
+	public void setClassName(String className) {
+		this.className = className;
+	}
+	public String[] getListOfModules() {
+		return listOfModules;
+	}
+	public void setListOfModules(String[] listOfModules) {
+		this.listOfModules = listOfModules;
+	}
+	public String[] getListOfStundentsEnrolled() {
+		return listOfStundentsEnrolled;
+	}
+	public void setListOfStundentsEnrolled(String[] listOfStundentsEnrolled) {
+		this.listOfStundentsEnrolled = listOfStundentsEnrolled;
+	}
+	public DateTime getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(DateTime startDate) {
+		this.startDate = startDate;
+	}
+	public DateTime getCloseDate() {
+		return closeDate;
+	}
+	public void setCloseDate(DateTime closeDate) {
+		this.closeDate = closeDate;
+	}
 	
 }
